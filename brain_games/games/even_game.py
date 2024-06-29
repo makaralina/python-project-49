d44e43d5b02a07594b40061930d0bc7b9be53407
Alina Makarovskaya <makarovskaya1307@gmail.com>
2024-06-29 16:56:23 +0300

    Update even_game.py

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -1,17 +1,14 @@
 from random import randint
-from prompt import string
 from brain_games import engine
 
 
 def run_even_game():
     name = engine.welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    ROUNDS = 3
-    for _ in range(ROUNDS):
+    for _ in range(engine.ROUNDS):
         random_number = randint(1, 100)
         correct_answer = 'yes' if random_number % 2 == 0 else 'no'
-        print(f'Question: {random_number}')
-        answer = string('Your answer: ')
+        answer = engine.get_answer(random_number)
         result = engine.compare_answers(answer, correct_answer, name)
         if result is False:
             break
