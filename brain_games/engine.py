4b2dcea2ec184b51dd4bf2c4b2962838a3cea25e
Alina Makarovskaya <makarovskaya1307@gmail.com>
2024-06-29 17:50:28 +0300

    Refactor even_game.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,27 +1,34 @@
 from prompt import string
+from random import randint
 
 
 ROUNDS = 3
+GREETING = 'Welcome to the Brain Games!'
+LOWER_BOUND = 1
+UPPER_BOUND = 100
 
 
-def greet():
-    print('Welcome to the Brain Games!')
-
-
-def welcome_user():
-    greet()
+def greet_and_get_name():
+    """Greets the user and returns his name"""
+    print(GREETING)
     name = string('May I have your name? ')
     print(f'Hello, {name}!')
     return name
 
 
 def get_answer(data):
+    """Asks the question to the user and returns the answer"""
     print(f'Question: {data}')
     answer = string('Your answer: ')
     return answer
 
 
+def get_random_number():
+    return randint(LOWER_BOUND, UPPER_BOUND)
+
+
 def compare_answers(player_answer, correct_answer, player_name):
+    """Compares the user's answer with the correct answer"""
     if player_answer == correct_answer:
         print('Correct!')
     else:
@@ -29,3 +36,4 @@ def compare_answers(player_answer, correct_answer, player_name):
               f"Correct answer was '{correct_answer}'.", sep=' ')
         print(f"Let's try again, {player_name}!")
         return False
+    return True
--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -1,16 +1,32 @@
-from random import randint
 from brain_games import engine
 
 
+game_hint = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def is_even(number):
+    return number % 2 == 0
+
+
+def get_correct_answer(number):
+    correct_answer = 'yes' if is_even(number) else 'no'
+    return correct_answer
+
+
+def play_round(name):
+    random_number = engine.get_random_number()
+    correct_answer = get_correct_answer(random_number)
+    answer = engine.get_answer(random_number)
+    result = engine.compare_answers(answer, correct_answer, name)
+    return result
+
+
 def run_even_game():
-    name = engine.welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    name = engine.greet_and_get_name()
+    print(game_hint)
     for _ in range(engine.ROUNDS):
-        random_number = randint(1, 100)
-        correct_answer = 'yes' if random_number % 2 == 0 else 'no'
-        answer = engine.get_answer(random_number)
-        result = engine.compare_answers(answer, correct_answer, name)
-        if result is False:
+        round_result = play_round(name)
+        if round_result is False:
             break
     else:
         print(f'Congratulations, {name}!')
