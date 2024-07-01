c44fa2290915b925370451dc503182099c3b4cf4
Alina Makarovskaya <makarovskaya1307@gmail.com>
2024-07-01 20:18:11 +0300

    Add brain_gcd.py

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games import gcd_game
+
+
+def main():
+    """Run the game of finding the greatest common divisor for two numbers"""
+    gcd_game.run_gcd_game()
+
+
+if __name__ == '__main__':
+    main()
