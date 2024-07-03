551bf44f2feb7364ce1e1e3b8830a3b29b80136d
Alina Makarovskaya <makarovskaya1307@gmail.com>
2024-07-03 21:08:02 +0300

    Add brain_prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games import prime_game
+
+
+def main():
+    """Run the game to check if the number is prime"""
+    prime_game.run_prime_game()
+
+
+if __name__ == '__main__':
+    main()
