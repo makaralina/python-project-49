449009e5c11fa7dc19bc83f4a75149c160f3bd5d
Alina Makarovskaya <makarovskaya1307@gmail.com>
2024-07-13 17:49:09 +0300

    Fix calc.py and brain_calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,6 +1,6 @@
-from brain_games import engine
-from random import choice
+from random import randint, choice
 from operator import add, sub, mul
+from brain_games.games.const import LOWER_BOUND, UPPER_BOUND
 
 
 GAME_HINT = 'What is the result of the expression?'
@@ -14,8 +14,8 @@ def get_random_operator():
 
 def get_random_numbers():
     """Get random positive numbers"""
-    random_number_1 = engine.get_random_number()
-    random_number_2 = engine.get_random_number()
+    random_number_1 = randint(LOWER_BOUND, UPPER_BOUND)
+    random_number_2 = randint(LOWER_BOUND, UPPER_BOUND)
     return random_number_1, random_number_2
 
 
@@ -29,27 +29,11 @@ def get_expression_result(a, b, operator):
         return mul(a, b)
 
 
-def play_round(name):
-    """Generate a random expression, get the user's answer
-    and compare it with the correct answer"""
+def play_round():
+    """Generate a random arithmetic expression and calculates the result"""
     operator = get_random_operator()
     random_number_1, random_number_2 = get_random_numbers()
-    expression = f'{random_number_1} {operator} {random_number_2}'
     correct_answer = str(get_expression_result(random_number_1, random_number_2,
                                                operator))
-    answer = engine.get_answer(expression)
-    result = engine.compare_answers(answer, correct_answer, name)
-    return result
-
-
-def run_calc_game():
-    """Play three rounds of the calculator game,
-    if the player's answer is incorrect - end the game early"""
-    name = engine.greet_and_get_name()
-    print(GAME_HINT)
-    for _ in range(engine.ROUNDS):
-        round_result = play_round(name)
-        if round_result is False:
-            break
-    else:
-        print(f'Congratulations, {name}!')
+    expression = f'{random_number_1} {operator} {random_number_2}'
+    return correct_answer, expression
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+from brain_games.engine import start_game
 from brain_games.games import calc
 
 
 def main():
     """Run the calculator game"""
-    calc.run_calc_game()
+    start_game(calc)
 
 
 if __name__ == '__main__':
