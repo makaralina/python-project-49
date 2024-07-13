ffdcb3709c1c87acccfbc605e089f70b6e5c24d1
Alina Makarovskaya <makarovskaya1307@gmail.com>
2024-07-13 17:50:10 +0300

    Fix even.py and brain_even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,4 +1,5 @@
-from brain_games import engine
+from random import randint
+from brain_games.games.const import LOWER_BOUND, UPPER_BOUND
 
 
 GAME_HINT = 'Answer "yes" if the number is even, otherwise answer "no".'
@@ -15,24 +16,9 @@ def get_correct_answer(number):
     return correct_answer
 
 
-def play_round(name):
+def play_round():
     """Generate a random number, get the user's answer to the question
     'Is it an even number?' and compare it with the correct answer"""
-    random_number = engine.get_random_number()
+    random_number = randint(LOWER_BOUND, UPPER_BOUND)
     correct_answer = get_correct_answer(random_number)
-    answer = engine.get_answer(random_number)
-    result = engine.compare_answers(answer, correct_answer, name)
-    return result
-
-
-def run_even_game():
-    """Play three rounds of the parity check game,
-    if the player's answer is incorrect - end the game early"""
-    name = engine.greet_and_get_name()
-    print(GAME_HINT)
-    for _ in range(engine.ROUNDS):
-        round_result = play_round(name)
-        if round_result is False:
-            break
-    else:
-        print(f'Congratulations, {name}!')
+    return correct_answer, str(random_number)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+from brain_games.engine import start_game
 from brain_games.games import even
 
 
 def main():
     """Run the parity check game"""
-    even.run_even_game()
+    start_game(even)
 
 
 if __name__ == '__main__':
