f3c47642f012552d6e0c0f9486458601d64ebd40
Alina Makarovskaya <makarovskaya1307@gmail.com>
2024-07-13 17:51:01 +0300

    Fix gcd.py and brain_gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,4 +1,5 @@
-from brain_games import engine
+from random import randint
+from brain_games.games.const import LOWER_BOUND, UPPER_BOUND
 
 
 GAME_HINT = 'Find the greatest common divisor of given numbers.'
@@ -6,8 +7,8 @@ GAME_HINT = 'Find the greatest common divisor of given numbers.'
 
 def get_random_numbers():
     """Get two random numbers"""
-    random_number_1 = engine.get_random_number()
-    random_number_2 = engine.get_random_number()
+    random_number_1 = randint(LOWER_BOUND, UPPER_BOUND)
+    random_number_2 = randint(LOWER_BOUND, UPPER_BOUND)
     return random_number_1, random_number_2
 
 
@@ -19,26 +20,10 @@ def find_gcd(a, b):
     return a
 
 
-def play_round(name):
+def play_round():
     """Generate a pair of random numbers to calculate 'gcd',
     get the user's answer and compare it with the correct answer"""
     a, b = get_random_numbers()
     expression = f'{a} {b}'
     correct_answer = str(find_gcd(a, b))
-    answer = engine.get_answer(expression)
-    result = engine.compare_answers(answer, correct_answer, name)
-    return result
-
-
-def run_gcd_game():
-    """Play three rounds of the game to find the greatest common
-    divisor of two numbers, if the player's answer is incorrect
-    - end the game early"""
-    name = engine.greet_and_get_name()
-    print(GAME_HINT)
-    for _ in range(engine.ROUNDS):
-        round_result = play_round(name)
-        if round_result is False:
-            break
-    else:
-        print(f'Congratulations, {name}!')
+    return correct_answer, expression
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+from brain_games.engine import start_game
 from brain_games.games import gcd
 
 
 def main():
     """Run the game of finding the greatest common divisor for two numbers"""
-    gcd.run_gcd_game()
+    start_game(gcd)
 
 
 if __name__ == '__main__':
