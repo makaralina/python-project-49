1b6026b264063789248990ec0c991aa2b95946a9
Alina Makarovskaya <makarovskaya1307@gmail.com>
2024-07-13 17:51:53 +0300

    Fix prime.py and brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+from brain_games.engine import start_game
 from brain_games.games import prime
 
 
 def main():
     """Run the game to check if the number is prime"""
-    prime.run_prime_game()
+    start_game(prime)
 
 
 if __name__ == '__main__':
