94c97783f04c7670041349e863b3af6c4d9667bb
Alina Makarovskaya <makarovskaya1307@gmail.com>
2024-07-13 17:52:44 +0300

    Fix progression.py and brain_progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,4 +1,4 @@
-from brain_games import engine
+from random import randint
 
 
 GAME_HINT = 'What number is missing in the progression?'
@@ -6,19 +6,19 @@ GAME_HINT = 'What number is missing in the progression?'
 
 def get_random_length():
     """Get the random length of the progression"""
-    random_length = engine.get_random_number(LOWER_BOUND=5, UPPER_BOUND=10)
+    random_length = randint(5, 10)
     return random_length
 
 
 def get_random_start():
     """Get the random start of the progression"""
-    random_start = engine.get_random_number(LOWER_BOUND=0, UPPER_BOUND=20)
+    random_start = randint(0, 20)
     return random_start
 
 
 def get_random_difference():
     """Get the random progression difference"""
-    random_difference = engine.get_random_number(LOWER_BOUND=1, UPPER_BOUND=9)
+    random_difference = randint(1, 9)
     return random_difference
 
 
@@ -27,22 +27,17 @@ def get_random_progression():
     progression = []
     length = get_random_length()
     start = get_random_start()
-    next_number = start
     difference = get_random_difference()
-    for _ in range(length):
-        if len(progression) == 0:
-            progression.append(start)
-        else:
-            next_number += difference
-            progression.append(next_number)
+    end = start + (length - 1) * difference
+    for i in range(start, end + 1, difference):
+        progression.append(i)
     return progression
 
 
 def get_random_index(progression):
     """Get the random index to hide a number in the progression"""
     length_progression = len(progression) - 1
-    random_index = engine.get_random_number(LOWER_BOUND=0,
-                                            UPPER_BOUND=length_progression)
+    random_index = randint(0, length_progression)
     return random_index
 
 
@@ -59,7 +54,7 @@ def get_progression_with_gap(progression, random_index):
     return progression_with_gap
 
 
-def play_round(name):
+def play_round():
     """Generate a random progression with a missing value,
     get the user's answer and compare it with the correct answer"""
     progression = get_random_progression()
@@ -67,20 +62,4 @@ def play_round(name):
     hidden_number = get_hidden_number(progression, random_index)
     progression_with_gap = get_progression_with_gap(progression, random_index)
     correct_answer = str(hidden_number)
-    answer = engine.get_answer(progression_with_gap)
-    result = engine.compare_answers(answer, correct_answer, name)
-    return result
-
-
-def run_progression_game():
-    """Play three rounds of the game to guess the hidden number
-    in the progression, if the player's answer is incorrect
-    - end the game early"""
-    name = engine.greet_and_get_name()
-    print(GAME_HINT)
-    for _ in range(engine.ROUNDS):
-        round_result = play_round(name)
-        if round_result is False:
-            break
-    else:
-        print(f'Congratulations, {name}!')
+    return correct_answer, progression_with_gap
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+from brain_games.engine import start_game
 from brain_games.games import progression
 
 
 def main():
     """Run the parity check game"""
-    progression.run_progression_game()
+    start_game(progression)
 
 
 if __name__ == '__main__':
